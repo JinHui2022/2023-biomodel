3064957f440c8710eb960c994f5beaa746d5dec6
JinHui2022 <106484837+JinHui2022@users.noreply.github.com>
2023-11-22 10:11:49 +0800

    fix some errors

--- project/file_management.py
+++ project/file_management.py
@@ -1,6 +1,8 @@
 ## this file offer functions to store data in specific form 
 ## and parse the data stored in certain file
 
+import numpy as np
+
 def save_place_field(dictionary,file_path):
     with open(file_path, 'w') as file:
         for key, value in dictionary.items():
@@ -10,6 +12,10 @@ def save_place_field(dictionary,file_path):
 def read_place_field(file_path):
     return None
 
-def read_spike_train(file_path):
+def read_spike_train(file_path, allow_pickle):
     ## numpy can take the task of saving spike trains
-    return None
+    data = np.load(file_path, allow_pickle=allow_pickle)
+    arrays = {}
+    for key in data.keys():
+        arrays[key] = data[key]
+    return arrays

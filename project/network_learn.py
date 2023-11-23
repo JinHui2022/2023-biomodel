3369babc78433ef1a1dc3f0815a85bab5a9ec042
JinHui2022 <106484837+JinHui2022@users.noreply.github.com>
2023-11-23 09:17:54 +0800

    network_learn uupdate

--- project/network_learn.py
+++ project/network_learn.py
@@ -7,3 +7,16 @@ Loads in hippocampal like spike train (produced by `generate_spike_trains.py`) a
 updated to produce symmetric STDP curve as reported in Mishra et al. 2016 - 10.1038/ncomms11552
 """
 
+import numpy as np
+import brainpy as bp
+import brainpy.math as bm
+import matplotlib.pyplot as plt
+from classes import STDP
+from parameter import *
+
+def run_STDP(spiking_neurons, spike_times, tau_s, tau_t, A1, A2, wmax, w_init):
+    # define pre, post neurons and connection
+    pre=bp.neurons.AdExIF(n_PC)
+    post=bp.neurons.AdExIF(n_PC)
+    syn=STDP(pre,post,bp.connect.All2All())
+    net=bp.Network(pre,syn,post)

739a8f64db119d67691dea590f8d8a8b468fc46d
LLPinkie <lejun@mail.ustc.edu.cn>
2023-11-28 18:49:16 +0800

    1

--- project/network_simulate.py
+++ project/network_simulate.py
@@ -1,2 +1,8 @@
 ## using the weight matrix produced in network_learn.py to construct a neuron
 ## network to simulate the SWRs 
+import brainpy as bp
+import brainpy.math as bm
+class EINet(bp.Network):
+    def __init__(self,num_exc,num_inh,method='exp_auto',**kwargs):
+        super(EINet, self).__init__(**kwargs)
+        pars = dict()

c61bcc45f83d8ef37736c7889036f86cb329e371
JinHui2022 <106484837+JinHui2022@users.noreply.github.com>
2023-12-13 09:28:36 +0800

    fix simulate

--- project/classes.py
+++ project/classes.py
@@ -82,27 +82,31 @@ class ca3simu(bp.Network):
         BCs=bp.neurons.AdExIF(n_BC, V_rest=Vrest_BC,V_reset=Vreset_BC,V_th=spike_th_BC,V_T=theta_BC,
                               delta_T=delta_T_BC,a=a_BC,b=b_BC,R=Rm_BC,tau=tau_mem_BC,tau_w=tau_w_BC,tau_ref=tref_BC)
         
+        # initialize
+        PCs.V[:]=Vrest_PC 
+        BCs.V[:]= Vrest_BC
+        
         # construct the connections
         ## MF -> PC
-        self.MF2PC=DualExponential(I_MF,PCs,bp.conn.One2One(),g_max=z*w_PC_MF,tau_decay=decay_PC_MF,tau_rise=rise_PC_MF,
+        self.MF2PC=DualExponential(I_MF,PCs,bp.conn.One2One(),g_max=norm_PC_MF*w_PC_MF,tau_decay=decay_PC_MF,tau_rise=rise_PC_MF,
                                    delay_step=delay_PC_E/dt,output=bp.synouts.COBA(Erev_E))
         ## PC -> PC
-        self.PC_E=DualExponential(PCs,PCs,conn_PC_E,g_max=z*w_PC_E,tau_decay=decay_PC_E,tau_rise=rise_PC_E,
+        self.PC_E=DualExponential(PCs,PCs,conn_PC_E,g_max=norm_PC_E*w_PC_E,tau_decay=decay_PC_E,tau_rise=rise_PC_E,
                                    delay_step=delay_PC_E/dt,output=bp.synouts.COBA(Erev_E))
         
         ## BC -> PC
         conn_PC_I=bp.conn.FixedProb(prob=connection_prob_BC,include_self=False,seed=42)
-        self.PC_I=DualExponential(BCs,PCs,conn_PC_I,g_max=z*w_PC_I,tau_decay=decay_PC_I,tau_rise=rise_PC_I,
+        self.PC_I=DualExponential(BCs,PCs,conn_PC_I,g_max=norm_BC_E*w_PC_I,tau_decay=decay_PC_I,tau_rise=rise_PC_I,
                                     delay_step=delay_PC_E/dt,output=bp.synouts.COBA(Erev_I))
         
         ## PC -> BC
-        conn_BC_E=bp.conn.FixedProb(prob=connection_prob_PC,include_self=False,seed=seed//13)
-        self.BC_E=DualExponential(PCs,BCs,conn_BC_E,g_max=z*w_BC_E,tau_decay=decay_BC_E,tau_rise=rise_BC_E,
+        conn_BC_E=bp.conn.FixedProb(prob=connection_prob_PC,include_self=False,seed=13)
+        self.BC_E=DualExponential(PCs,BCs,conn_BC_E,g_max=norm_PC_I*w_BC_E,tau_decay=decay_BC_E,tau_rise=rise_BC_E,
                                   delay_step=delay_BC_E/dt,output=bp.synouts.COBA(Erev_E))
         
         ## BC -> BC
-        conn_BC_I=bp.conn.FixedProb(prob=connection_prob_BC,include_self=False,seed=seed//7)
-        self.BC_I=DualExponential(BCs,BCs,conn_BC_I,g_max=z*w_BC_I,tau_decay=decay_BC_I,tau_rise=rise_BC_I,
+        conn_BC_I=bp.conn.FixedProb(prob=connection_prob_BC,include_self=False,seed=7)
+        self.BC_I=DualExponential(BCs,BCs,conn_BC_I,g_max=norm_BC_I*w_BC_I,tau_decay=decay_BC_I,tau_rise=rise_BC_I,
                                   delay_step=delay_BC_I/dt,output=bp.synouts.COBA(Erev_I))
         
         ## store the variables in all these neuron group
--- project/network_simulate.py
+++ project/network_simulate.py
@@ -12,14 +12,15 @@ def run_ca3simu(dur,freq,conn_PC,weight_matrix_PC,mode,seed):
     net=ca3simu(freq=freq,conn_PC_E=conn_PC,wmx_PC_E=weight_matrix_PC,mode=mode,seed=seed)
     runner=bp.DSRunner(
         net,
-        monitors=['PCs.spike']
+        monitors=['PCs.spike','MF.spike']
     )
     runner(dur)
 
     ts=runner.mon.ts
     PC_spikes=runner.mon['PCs.spike']
+    MF_spikes=runner.mon['MF.spike']
 
-    return ts, PC_spikes
+    return ts, PC_spikes,MF_spikes
 
 # input file name
 header=".\data\\asym_"
@@ -36,19 +37,19 @@ weight_PC/=4 # rescale
 freq=rate_MF
 mode="asym"
 seed=1234
-dur=5000 ## ms
+dur=2000 ## ms
 
 # to run
 wmx_PC=np.zeros((n_PC,n_PC))
 wmx_PC[pre_id,post_id]=weight_PC
 conn=bp.conn.IJConn(i=pre_id,j=post_id)
 conn = conn(pre_size=n_PC, post_size=n_PC)
-ts,PC_spikes=run_ca3simu(dur=dur,freq=freq,conn_PC=conn,weight_matrix_PC=wmx_PC,mode=mode,seed=seed)
+ts,PC_spikes,MF_spikes=run_ca3simu(dur=dur,freq=freq,conn_PC=conn,weight_matrix_PC=wmx_PC,mode=mode,seed=seed)
 
 # to plot
 fig,gs=plt.subplots()
 
-bp.visualize.raster_plot(ts,PC_spikes,markersize=0.1)
+bp.visualize.raster_plot(ts,PC_spikes,markersize=0.5)
 plt.title('just for test')
 plt.ylabel("Neuron Index")
 plt.show()
